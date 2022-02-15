85e042f9f261d55b20b7a4d26630737c9bf86d03
Jasper <JasperLawliet@hotmail.com>
2022-02-15 00:56:07 -0500

    added survey page, with rendered view and link from landing page to survey.

---
+++ tmp/2eojmps41ww0o.y87iujn2dk1v.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Take the survey</title>
+</head>
+<body>
+<p>Take my midterm survey!</p>
+<p>First, enter your name: </p>
+
+</body>
+</html>
